c612f4fe78c8cb8fc180ca55f4c6e7b92ccdf335
ucfilho <42356381+ucfilho@users.noreply.github.com>
2021-02-03 12:25:31 -0300

    Update CNN_FIND_GRAIN_PAPER_STEP_02.py

--- CNN_FIND_GRAIN_PAPER_STEP_02.py
+++ CNN_FIND_GRAIN_PAPER_STEP_02.py
@@ -109,8 +109,8 @@ def AnnGrain(df,df_class):
   #])
 
   model = keras.Sequential()
-  model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28) )
-  model.add(layers.MaxPooling2D((2, 2))
+  model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28) ))
+  model.add(layers.MaxPooling2D((2, 2)))
   model.add(layers.Conv2D(64, (3, 3), activation='relu'))
   model.add(layers.MaxPooling2D((2, 2)))
   model.add(layers.Conv2D(64, (3, 3), activation='relu'))
