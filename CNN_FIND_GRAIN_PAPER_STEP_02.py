0fefdf00cade1a8cf6626f310649253540b69453
ucfilho <42356381+ucfilho@users.noreply.github.com>
2021-02-03 12:53:21 -0300

    Update CNN_FIND_GRAIN_PAPER_STEP_02.py

--- CNN_FIND_GRAIN_PAPER_STEP_02.py
+++ CNN_FIND_GRAIN_PAPER_STEP_02.py
@@ -118,7 +118,7 @@ def AnnGrain(df,df_class):
   model.add(tf.keras.layers.MaxPooling2D((2, 2)))
   model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
   model.add(tf.keras.layers.Flatten())
-  model.add(tf.keras.keras.layers.Dense(128, activation='relu'))
+  model.add(tf.keras.layers.Dense(128, activation='relu'))
   mode.add(tf.keras.layers.Dense(10, activation='softmax'))       
   
  #model = tf.keras.Sequential([
