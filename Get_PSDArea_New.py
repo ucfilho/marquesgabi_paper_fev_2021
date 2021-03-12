2bd07e0d6161890ed33a527c26666e56153fe5eb
ucfilho <42356381+ucfilho@users.noreply.github.com>
2021-03-12 18:07:15 -0300

    Update Get_PSDArea_New.py

--- Get_PSDArea_New.py
+++ Get_PSDArea_New.py
@@ -28,7 +28,7 @@ def PSDArea(df_size):
 
     for i in range(28):
       for j in range(28):
-        if img[i,j] < mean_value*1.1:
+        if img[i,j] < mean_value*1.05:
           img_new[i,j] = 255
         else:
           img_new[i,j] = 0
